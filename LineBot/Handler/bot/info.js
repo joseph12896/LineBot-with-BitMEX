f5b5e186e24f16ebd4c843d101c4e2ec1e45ccff
rageNami <gr7p4zxh@gmail.com>
2018-01-26 05:12:36 +0800

    disable etherscan related function

--- LineBot/Handler/bot/info.js
+++ LineBot/Handler/bot/info.js
@@ -9,16 +9,16 @@ module.exports = new wrapper(/^info$/ig, info);
 /**
  * 使用etherscan.io API查詢錢包餘額
  */
-var walletBalance = 0;
-setInterval(async function updateWalletBalance() {
-    try {
-        let res = await fetch('https://api.etherscan.io/api?module=account&action=balance&address=0xa087C9E3eAE348D16B95ea19719c67f77EDa7080&tag=latest&apikey=S518HCWQBFSCA3C9ERQXM4PJTIK45SZFI4')
-        res = await res.json();
-        if (res.message == 'OK') walletBalance = Number(res.result / 1000000000000000000).toFixed(3);
-    } catch (e) {
-        console.log(e)
-    }
-}, 10 * 1000);
+// var walletBalance = 0;
+// setInterval(async function updateWalletBalance() {
+//     try {
+//         let res = await fetch('https://api.etherscan.io/api?module=account&action=balance&address=0xa087C9E3eAE348D16B95ea19719c67f77EDa7080&tag=latest&apikey=S518HCWQBFSCA3C9ERQXM4PJTIK45SZFI4')
+//         res = await res.json();
+//         if (res.message == 'OK') walletBalance = Number(res.result / 1000000000000000000).toFixed(3);
+//     } catch (e) {
+//         console.log(e)
+//     }
+// }, 10 * 1000);
 
 /**
  * Info
